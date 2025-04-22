116f6d8c36853657799784e41a788310ab3f75a9
NavaneethaBonala <149959297+NavaneethaBonala@users.noreply.github.com>
2025-04-22 15:33:42 +0530

    Initial commit

---
+++ src/main/java/assignment05/NewTest.java
@@ -0,0 +1,15 @@
+package assignment05;
+
+public class NewTest {
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+
+	}
+}
